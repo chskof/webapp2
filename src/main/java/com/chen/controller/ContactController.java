5aeb34643bc9cf78189d1bb18212c20527f58965
chs <412603534@qq.com>
2019-11-24 17:36:36 +0800

    20191124

--- src/main/java/com/chen/controller/ContactController.java
+++ src/main/java/com/chen/controller/ContactController.java
@@ -3,22 +3,33 @@ package com.chen.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.chen.model.Contact;
 import com.chen.service.IFirstService;
 
+@RequestMapping("/contact/")
 @Controller
 public class ContactController {
 	@Autowired
 	private IFirstService firstService;
 	
-	@RequestMapping("/find.do")
-	public ModelAndView findById() {
-		ModelAndView mav = new ModelAndView("index");   
-        Contact person = firstService.FindById(1);
-        mav.addObject("perosn", person);   
-        return mav;
+	//页面跳转
+	@RequestMapping("/queryList.do")
+	public ModelAndView queryList(String test) {
+		System.out.println(test);
+		ModelAndView mav = new ModelAndView("success");
+		return mav;
 	}
 	
+	//Ajax数据交互
+	@RequestMapping("/find.do")
+	@ResponseBody
+	public Contact findById() {
+        Contact person = firstService.FindById(1);
+        return person;
+	}
+	
+
 }
--- src/main/java/com/chen/model/Contact.java
+++ src/main/java/com/chen/model/Contact.java
@@ -1,13 +1,21 @@
 package com.chen.model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 public class Contact implements Serializable {
-	private long id;
+	private Long id;
 	private String name;
-	private int age;
+	private String nameSpell;
 	private String phone;
 	private String address;
+	private Date birthday;
+	private String weixin;
+	private Long QQ;
+	private String mark1;
+	private Long mark2;
+	private Date mark3;
 	
 	public long getId() {
 		return id;
@@ -20,12 +28,6 @@ public class Contact implements Serializable {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
 	}
 	public String getPhone() {
 		return phone;
@@ -40,5 +42,47 @@ public class Contact implements Serializable {
 		this.address = address;
 	}
 	
-	
+	public String getNameSpell() {
+		return nameSpell;
+	}
+	public void setNameSpell(String nameSpell) {
+		this.nameSpell = nameSpell;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getWeixin() {
+		return weixin;
+	}
+	public void setWeixin(String weixin) {
+		this.weixin = weixin;
+	}
+
+	public String getMark1() {
+		return mark1;
+	}
+	public void setMark1(String mark1) {
+		this.mark1 = mark1;
+	}
+	public Long getMark2() {
+		return mark2;
+	}
+	public void setMark2(Long mark2) {
+		this.mark2 = mark2;
+	}
+	public Date getMark3() {
+		return mark3;
+	}
+	public void setMark3(Date mark3) {
+		this.mark3 = mark3;
+	}
+	public long getQQ() {
+		return QQ;
+	}
+	public void setQQ(long qQ) {
+		QQ = qQ;
+	}
 }
